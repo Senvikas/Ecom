20b26783c2df0928733a2d5d23633d1412dcebd9
Senvikas <senvikas706@gmail.com>
2025-01-20 10:20:47 +0530

    Shop By Type

--- EcomWeb/frontEnd/src/components/Home.jsx
+++ EcomWeb/frontEnd/src/components/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import HeroSection from './HeroSection.jsx';
+import CardGrid from './CardGridShopByType.jsx';
 const Home = () => {
   return (
     <div className="text-center">
       {/* <h1 className="text-4xl font-bold">Welcome to the Home Page</h1> */}
       <HeroSection />
+
+      {/* Card grid: Shop by Type */}
+      <div className="mt-40"> {/* Add margin between sections */}
+        <CardGrid />
+      </div>
     </div>
   );
 };
