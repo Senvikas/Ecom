c223db4025bc153745d37c60979740fd47569b66
Senvikas <senvikas706@gmail.com>
2025-01-21 10:04:15 +0530

    Top Brands

--- EcomWeb/frontEnd/src/components/Home.jsx
+++ EcomWeb/frontEnd/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeroSection from './HeroSection.jsx';
 import CardGrid from './CardGridShopByType.jsx';
+import BrandGrid from './BrandGrid.jsx';
 const Home = () => {
   return (
     <div className="text-center">
@@ -11,6 +12,8 @@ const Home = () => {
       <div className="mt-40"> {/* Add margin between sections */}
         <CardGrid />
       </div>
+
+      <BrandGrid /> {/* Add BrandGrid below the "Shop By Type" section */}
     </div>
   );
 };
